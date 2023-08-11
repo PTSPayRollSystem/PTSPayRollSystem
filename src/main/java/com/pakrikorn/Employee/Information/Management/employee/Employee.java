890b72d75372a8f05181aecba43fa0996cca8685
Siva Chaitanya Tallapudi <sivachaitanya@Sivas-MacBook-Air.local>
2023-08-11 16:08:09 +0530

    updated code

--- src/main/java/com/pakrikorn/Employee/Information/Management/employee/Employee.java
+++ src/main/java/com/pakrikorn/Employee/Information/Management/employee/Employee.java
@@ -2,21 +2,13 @@ package com.pakrikorn.Employee.Information.Management.employee;
 
 import java.time.LocalDate;
 
-import org.antlr.v4.runtime.misc.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -42,7 +34,7 @@ public class Employee {
     private String lastName;
     private String address;
     private String contactNumber;
-    
+  
     private String emailAddress;
     @JsonFormat(pattern="dd-mm-yyyy")
     private LocalDate dateOfBirth;
